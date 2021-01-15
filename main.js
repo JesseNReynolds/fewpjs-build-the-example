1c5cbc23a371b8d81d62def0a205a0af997d98db
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-15 12:33:17 -0700

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.getElementById('modal')
 
-// Your JavaScript code goes here!
 
+// Your JavaScript code goes here
+// Invocations
+listenForLikes()
+
+
+// Select all like buttons and add event listeners
+
+
+function listenForLikes() {
+  const hearts = document.getElementsByClassName('like-glyph')
+  
+  Array.from(hearts).forEach(function(element) {
+    element.addEventListener('click', (event) => clickLike(event))
+  })
+}
+
+// Handle click
+
+function clickLike(event) {
+  const element = event.target
+  
+  if (!element.classList.contains('activated-heart')) {
+    mimicServerCall()
+      .then(function () {
+        element.classList.add('activated-heart')
+        element.innerHTML = FULL_HEART
+      })
+      .catch(function() {
+        errorModal.classList.remove('hidden')
+        setTimeout(function(rejection) {
+          errorModal.classList.add('hidden');
+          errorModal.innerHTML = rejection;
+        },
+           5000)
+      })
+  }
+
+}
+
+// error modal handling
+
+
+// function showError() {
+//   errorModal.classList.remove('hidden')
+//   setTimeout(errorModal.classList.add('hidden'), 5000)
+// }
 
 
 
